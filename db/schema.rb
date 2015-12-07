b805a24bef9e7f74bad48b4cf0bd3355c41c43d3
Logan McDonald <loganmcdona11@gmail.com>
2015-12-07 11:33:20 -0800

    Devise up and working, added a homepage that allows you to sign out, github login and normal login work as well

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207183322) do
+ActiveRecord::Schema.define(version: 20151207184601) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20151207183322) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
