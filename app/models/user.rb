02aed5b74e542f55f60c18bca1327f1c7ccf9185
Logan McDonald <loganmcdona11@gmail.com>
2015-12-07 12:47:42 -0800

    Fixed github login when email ogin already exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:github]
+
   def self.from_omniauth(auth)
+    where(email: auth.info.email).each do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      return user
+    end
+    
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
