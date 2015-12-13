66c8559a1defb13e66227f785fc006ed688c115e
Ricky Hougland <houglande@gmail.com>
2015-12-12 23:29:46 -0800

    create join table for resources and tags, and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207211435) do
+ActiveRecord::Schema.define(version: 20151213072720) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20151207211435) do
     t.integer  "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "resources_tags", force: :cascade do |t|
+    t.integer "resource_id"
+    t.integer "tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
